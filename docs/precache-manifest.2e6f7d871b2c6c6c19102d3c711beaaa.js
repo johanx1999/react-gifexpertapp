46ade239aed41c43dd8f6d5688daff22104aaece
Johan Bedoya <nexus1999x@gmail.com>
2020-08-17 20:01:12 -0500

    Docs Agregado

---
+++ docs/precache-manifest.2e6f7d871b2c6c6c19102d3c711beaaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c685f7a501e5332437938f26197371b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2866c24310eab782d102",
+    "url": "/static/css/main.fa052a41.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2866c24310eab782d102",
+    "url": "/static/js/main.165e7f2e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
